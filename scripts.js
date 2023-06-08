4440ab80644fd793bba90e6e96e17660406fef8a
Fernando Afonso <fernandoafonsotrp@outlook.com>
2023-06-08 12:17:33 -0300

    Tipos de saida em Javascript

--- scripts.js
+++ scripts.js
@@ -1,1 +1,22 @@
+/* Tipos de saida 
+alert("Sou uma alerta!")
 console.log("Hello Word - Fernando Gates");
+document.getElementById("texto").innerHTML="Meu primeiro texto em <b>JS</b>";
+document.write('Oi escrito com <b>document.write</b>');
+*/ 
+
+/* Java script aceita quebra de linha e desconsidera espaços */
+
+function soma(){
+
+let a,b,c;
+a=5;
+b=6;
+c=7;
+
+var result = a + b + c;
+}
+
+
+
+console.log(result);
